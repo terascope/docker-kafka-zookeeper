b1f15768e464bf5d3b49a808c177a8b0bb813f4c
peterdemartini <thepeterdemartini@gmail.com>
2018-11-07 15:01:29 -0700

    fix issues with running locally

--- assets/scripts/start-kafka.sh
+++ assets/scripts/start-kafka.sh
@@ -8,18 +8,18 @@
 # * LOG_RETENTION_BYTES: configure the size at which segments are pruned from the log, (default is 1073741824, for 1GB)
 # * NUM_PARTITIONS: configure the default number of log partitions per topic
 
-# Set the external host and port
-if [ -n "$ADVERTISED_HOST" ]; then
-    echo "advertised host: $ADVERTISED_HOST"
-    # Uncomment advertised.listeners
-    sed -r -i 's/^(#)(advertised.listeners)/\2/g' "$KAFKA_HOME/config/server.properties"
+BROKER_PORT="${ADVERTISED_PORT:-9092}"
+SERVER_CONFIG="$KAFKA_HOME/config/server.properties"
 
-    # Replace your.host.name with $ADVERTISED_HOST
-    sed -r -i "s/your.host.name/$ADVERTISED_HOST/g" "$KAFKA_HOME/config/server.properties"
-fi
-if [ -n "$ADVERTISED_PORT" ]; then
-    echo "advertised port: $ADVERTISED_PORT"
-    sed -r -i "s/#(advertised.port)=(.*)/\\1=$ADVERTISED_PORT/g" "$KAFKA_HOME/config/server.properties"
+if [ -n "$ADVERTISED_PORT" ] || [ -n "$ADVERTISED_HOST" ]; then
+    echo "setting advertised and port: $ADVERTISED_HOST:$BROKER_PORT"
+    {
+        printf "\\nadvertised.port = %s" "$BROKER_PORT"
+        printf "\\nlisteners = INSIDE://:9092,OUTSIDE://:%s" "$ADVERTISED_HOST" "$BROKER_PORT"
+        printf "\\nadvertised.listeners = INSIDE://:9092,OUTSIDE://%s:%s" "$ADVERTISED_HOST" "$BROKER_PORT" 
+        printf "\\nlistener.security.protocol.map = INSIDE:PLAINTEXT,OUTSIDE:PLAINTEXT"
+        printf "\\ninter.broker.listener.name = INSIDE"
+    } >> "$SERVER_CONFIG"
 fi
 
 # Set the zookeeper chroot
@@ -36,10 +36,10 @@ if [ ! -z "$ZK_CHROOT" ]; then
     }
 
     # configure kafka
-    sed -r -i "s/(zookeeper.connect)=(.*)/\\1=localhost:$ZOOKEEPER_PORT\\/$ZK_CHROOT/g" "$KAFKA_HOME/config/server.properties"
+    sed -r -i "s/(zookeeper.connect)=(.*)/\\1=localhost:$ZOOKEEPER_PORT\\/$ZK_CHROOT/g" "$SERVER_CONFIG"
 else
     # configure kafka
-    sed -r -i "s/(zookeeper.connect)=(.*)/\\1=localhost:$ZOOKEEPER_PORT/g" "$KAFKA_HOME/config/server.properties"
+    sed -r -i "s/(zookeeper.connect)=(.*)/\\1=localhost:${ZOOKEEPER_PORT:-2181}/g" "$SERVER_CONFIG"
 fi
 
 
@@ -47,24 +47,24 @@ fi
 # Allow specification of log retention policies
 if [ ! -z "$LOG_RETENTION_HOURS" ]; then
     echo "log retention hours: $LOG_RETENTION_HOURS"
-    sed -r -i "s/(log.retention.hours)=(.*)/\\1=$LOG_RETENTION_HOURS/g" "$KAFKA_HOME/config/server.properties"
+    sed -r -i "s/(log.retention.hours)=(.*)/\\1=$LOG_RETENTION_HOURS/g" "$SERVER_CONFIG"
 fi
 if [ ! -z "$LOG_RETENTION_BYTES" ]; then
     echo "log retention bytes: $LOG_RETENTION_BYTES"
-    sed -r -i "s/#(log.retention.bytes)=(.*)/\\1=$LOG_RETENTION_BYTES/g" "$KAFKA_HOME/config/server.properties"
+    sed -r -i "s/#(log.retention.bytes)=(.*)/\\1=$LOG_RETENTION_BYTES/g" "$SERVER_CONFIG"
 fi
 
 # Configure the default number of log partitions per topic
 if [ ! -z "$NUM_PARTITIONS" ]; then
     echo "default number of partition: $NUM_PARTITIONS"
-    sed -r -i "s/(num.partitions)=(.*)/\\1=$NUM_PARTITIONS/g" "$KAFKA_HOME/config/server.properties"
+    sed -r -i "s/(num.partitions)=(.*)/\\1=$NUM_PARTITIONS/g" "$SERVER_CONFIG"
 fi
 
 # Enable/disable auto creation of topics
 if [ ! -z "$AUTO_CREATE_TOPICS" ]; then
     echo "auto.create.topics.enable: $AUTO_CREATE_TOPICS"
-    echo "\\nauto.create.topics.enable=$AUTO_CREATE_TOPICS" >> "$KAFKA_HOME/config/server.properties"
+    printf "\\nauto.create.topics.enable=%s" "$AUTO_CREATE_TOPICS" >> "$SERVER_CONFIG"
 fi
 
 # Run Kafka
-"$KAFKA_HOME/bin/kafka-server-start.sh" "$KAFKA_HOME/config/server.properties"
+"$KAFKA_HOME/bin/kafka-server-start.sh" "$SERVER_CONFIG"
